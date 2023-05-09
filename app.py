297cfe8129fd36b9a141ce04e2a85c71fd5fced0
Leandro Felix <lehinfo.felix@gmail.com>
2023-05-09 19:12:12 -0300

    state: project base

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+from routes.routes import routes_bp
+
+app = Flask(__name__, static_folder="static")
+
+app.register_blueprint(routes_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
---
+++ routes/routes.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+routes_bp = Blueprint("routes", __name__)
+
+
+@routes_bp.route("/")
+def index():
+    return render_template("index.html", page_title='Início')
