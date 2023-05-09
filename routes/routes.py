38ae538bddd4852d3206b8d3f89fa1704548a2be
Leandro Felix <lehinfo.felix@gmail.com>
2023-05-09 19:55:19 -0300

    state: base da estrutura do projeto

--- routes/routes.py
+++ routes/routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template
 
 routes_bp = Blueprint("routes", __name__)
 
-
 @routes_bp.route("/")
 def index():
     return render_template("index.html", page_title='Início')
