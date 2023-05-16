7b20e95aa653e36bb886391812ee0ff0a2273434
Leandro Félix <58048297+lehinfo-felix@users.noreply.github.com>
2023-05-15 22:56:14 -0300

    finalização da atv

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 from routes.routes import routes_bp
 
-app = Flask(__name__, static_folder="static")
+app = Flask(
+    __name__,
+    static_folder="static",
+    template_folder="templates")
 
 app.register_blueprint(routes_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(
+        debug=True,
+        host="0.0.0.0")
--- routes/routes.py
+++ routes/routes.py
@@ -5,3 +5,19 @@ routes_bp = Blueprint("routes", __name__)
 @routes_bp.route("/")
 def index():
     return render_template("index.html", page_title='Início')
+
+@routes_bp.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html", page_title='Cadastro de usuários')
+
+@routes_bp.route("/sobre")
+def sobre():
+    return render_template("sobre.html", page_title='Sobre o sistema')
+
+@routes_bp.route("/login")
+def login():
+    return render_template("login.html", page_title='Perfil de administrador')
+
+@routes_bp.route("/sucesso")
+def sucesso():
+    return render_template("sucesso.html", page_title='Perfil de administrador')
